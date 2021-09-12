7cc39622f6557331d127a6cf00aa69023a95b633
Xala-ipman <89108413+Xala-ipman@users.noreply.github.com>
2021-09-12 19:32:14 +0300

    Create anti.js

---
+++ commands/general/anti.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js")
+module.exports = {
+  name: "anti",
+  aliases: [],
+  enabled: true,
+  memberPermissions: ["SEND_MESSAGES"],
+  botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+  ownerOnly: false,
+  cooldown: 3000,
+  run: async (bot, message, args, dev) => {
+    const embed = new MessageEmbed()
+      .setColor("#34eb46")
+      .setFooter(message.author.tag, message.author.displayAvatarURL())
+      .setDescription(`Type: [<number>,on,off]\n\n**anti-ban, anti-kick, anti-role, anti-channel, anti-bot, anti-spam`)
+    message.channel.send(embed);
+  }
+}
