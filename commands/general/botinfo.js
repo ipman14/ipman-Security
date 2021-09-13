30ebd84d934912dcce2cefc9b2be7c0dea42c89a
Xala-ipman <89108413+Xala-ipman@users.noreply.github.com>
2021-09-13 14:59:37 +0300

    Update botinfo.js

--- commands/general/botinfo.js
+++ commands/general/botinfo.js
@@ -42,12 +42,12 @@ API Latency: **${Math.round(bot.ping)}ms**
 Developed Language: **Javascript**
 Node.js Version: **V12**
 Discord.js Version: **v12.5.2**`);
-.setTitle("")
+.setTitle("Click Invite")
 .setURL(
-  ``
-.setTitle("")
+  `https://discord.com/api/oauth2/authorize?client_id=796022181654036501&permissions=8&scope=bot`
+.setTitle("Server Supporter")
 .setURL(
-  ``        
+  `https://discord.gg/Wy7K4hyZ5p `        
 
       m.edit(embed);
     });
