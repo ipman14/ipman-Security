549567b6f144f7510e7725d5b8abe3a8a1cb4c5b
ipman14 <90585113+ipman14@users.noreply.github.com>
2021-10-11 17:32:56 +0300

    Update help.js

--- commands/general/help.js
+++ commands/general/help.js
@@ -15,7 +15,7 @@ module.exports = {
   run: async (bot, message, args, dev) => {
     const embed = new MessageEmbed()
       .setFooter(message.author.tag, message.author.displayAvatarURL())
-      .setColor("#0000")
+      .setColor("#333332")
       .setTimestamp()
       .setThumbnail();
     embed
