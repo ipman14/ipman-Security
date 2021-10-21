9e2b378b7395ff26190f83e61f6128880b1f69da
ipman14 <90585113+ipman14@users.noreply.github.com>
2021-10-21 18:55:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,15 +55,10 @@ bot.on("ready", () => {
 
 /////
 
-const channelid = "899767634457939968"    //id channele vc
-bot.on("ready", () => {
-    const channel = client.channels.cache.get(channelid);
-    if (!channel) return console.error("The channel does not exist!");
-    channel.join().then(connection => {
-        console.log("Successfully connected.");
-    }).catch(e => {
-        console.error(e);
-    });
+
+bot.on('ready', () => {
+var channel = bot.channels.cache.get('899767634457939968');
+    if (channel) channel.join();
 });
 
 //////
